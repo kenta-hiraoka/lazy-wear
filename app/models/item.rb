dadc1f9316267f2316177daafb8ef41bf4488553
kenta-hiraoka <k.h.kentahiraoka@gmail.com>
2019-06-14 17:02:05 +0900

    fileupload

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+  has_many :images, dependent: :destroy
+    # enum status: {
+    #                   formal:             0,
+    #                   formal_casual:      1,
+    #                   casual:             2,
+    #                   casual_relux:       3,
+    #                   relux:              4,
+    #                   all:                5,
+    # }
 end
